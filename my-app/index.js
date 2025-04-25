ec912f1695efa265dd1102386c6b61cb29df9f29
Marcelo <marques.marcelo@gmail.com>
2025-04-25 10:17:56 -0300

    Remove arquivos .env do repositório

--- my-app/index.js
+++ my-app/index.js
@@ -1,11 +1,20 @@
-// index.js
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 app.get("/", (req, res) => {
   const id = fs.readFileSync("/etc/hostname", "utf8");
-  res.send(`<h1>Hello from container ${id}</h1>`);
+  
+  // Ler o arquivo environment-info.txt
+  const envInfo = fs.readFileSync(path.join(__dirname, 'info', 'environment-info.txt'), 'utf8');
+
+  res.send(`
+    <h1>Hello from container ${id}</h1>
+    <pre>${envInfo}</pre>  <!-- Exibe o conteúdo do environment-info.txt -->
+  `);
 });
 
-app.listen(80);
+app.listen(80, () => {
+  console.log("App listening on port 80");
+});
